69ee92c11693cdbcb17cbe71c53f6dba8491e4d5
ddooing <stpaq@naver.com>
2023-10-06 19:43:41 +0900

    home 구성 1차

--- example_sc/src/frame/Home.java
+++ example_sc/src/frame/Home.java
@@ -8,49 +8,62 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Home extends JPanel {
-	/*
-    private int currentImageIndex = 0;
-    
-    private String[] imagePaths = {
-            "./src/Between StarShine and Clay.jpg",
-            "./src/디올 성수.jpg",
-            "./src/라인프렌즈.jpg",
-            "./src/쿨라링.jpg"
-    };
-	*/
+
     private JLabel imageLabel;
-    private Timer imageTimer;
+    //private Timer imageTimer;
     ImageIcon icon;
 
     public Home() {
     	
 		
+    	// JPanel 구조 설정
+        setBackground(Color.white);
+        setLayout(null);
         setSize(400, 700);
-        setVisible(true);
+        
+        // 이미지 
+        	// 검색 아이콘
+        ImageIcon imageSearch = new ImageIcon("./src/30.png");
+        	//선
+        ImageIcon menuLine =new ImageIcon("./src/line3.png");
+        
+        	// 필터 4개 
+        ImageIcon one= new ImageIcon("./src/30.png");
+        ImageIcon two = new ImageIcon("./src/30.png");
+        ImageIcon three= new ImageIcon("./src/30.png");
+        ImageIcon four = new ImageIcon("./src/30.png");
         
         
-        ImageIcon imageSearch = new ImageIcon("./src/searchicon.png");
-
-        JButton btnGal = new JButton("전시회");
-        JButton btnPop = new JButton("팝업스토어");
-        JButton btnSearch = new JButton(imageSearch);
-
+        
+       // JButton btnGal = new JButton("전시회");
+        //JButton btnPop = new JButton("팝업스토어");
+        JButton btnSearch = new JButton(imageSearch);// 검색 버튼
+        JLabel menuline = new JLabel(menuLine);// 메뉴선
+        
+        JButton popula_btn = new JButton("");//인기 필터
+        JButton recent_btn = new JButton("");// 최신 필터
+        JButton free_btn = new JButton("");// 무료 필터
+        JButton soonEnd_btn = new JButton("");// 곧 종료 필터 
+        
+        
         JButton btnEnd = new JButton("");
         JButton btnBack = new JButton("");
         JButton btnHome = new JButton("");
         JButton btnTicket = new JButton("");
         JButton btnSoon = new JButton("");
         
-        
+        /*
         btnGal.setSize(200, 50);
-        btnGal.setLocation(-30, 25);
+        btnGal.setLocation(50, 25);
         btnGal.setFont(new Font("나눔스퀘어OTF Bold", Font.BOLD, 26));
         btnGal.setContentAreaFilled(false);
         btnGal.setBorderPainted(false);
@@ -62,13 +75,23 @@ public class Home extends JPanel {
         btnPop.setContentAreaFilled(false);
         btnPop.setBorderPainted(false);
         
-        
-        btnSearch.setSize(40,40);
-        btnSearch.setLocation(btnPop.getX() + btnPop.getWidth()+10, 30);
+        */
+        // 검색 아이콘
+        btnSearch.setSize(50,50);
+        btnSearch.setLocation(310, 40);
         btnSearch.setBorderPainted(false); //버튼 외각선 지우기
         btnSearch.setContentAreaFilled(false); //버튼 투명하게 지우기(이미지는 남음)
         btnSearch.setFocusPainted(false); //버튼 선택표시 지우기
-         
+        
+        // 선 
+        menuline.setSize(380, 20);
+        menuline.setLocation(10, btnSearch.getY() + btnSearch.getHeight());
+        //add(btnGal);
+        //add(btnPop);
+        add(btnSearch);
+        add(menuline);
+        
+        /*
         btnEnd.setBorderPainted(false);
         btnEnd.setContentAreaFilled(false);
         btnEnd.setFocusPainted(false);
@@ -88,7 +111,7 @@ public class Home extends JPanel {
         btnSoon.setBorderPainted(false);
         btnSoon.setContentAreaFilled(false);
         btnSoon.setFocusPainted(false);
-
+		*/
         int buttonWidth = 83;
         int buttonSpacing = 10;
         int yOffset = (int) getLocation().getY() / 2 + 630;
@@ -105,30 +128,16 @@ public class Home extends JPanel {
         btnHome.setLocation((getWidth() - totalWidth) / 2 + 2 * (buttonWidth + buttonSpacing), yOffset);
         btnTicket.setLocation((getWidth() - totalWidth) / 2 + 3 * (buttonWidth + buttonSpacing), yOffset);
         btnSoon.setLocation((getWidth() - totalWidth) / 2 + 4 * (buttonWidth + buttonSpacing), yOffset);
-
+        /*
         add(btnGal);
         add(btnPop);
         add(btnSearch);
-        
+        */
+        /*
      // 이미지 레이블 생성
         imageLabel = new JLabel();
         imageLabel.setBounds(40, btnGal.getY() + btnGal.getHeight() + 10, 400, 400); // 위치를 버튼 아래로 조절
         add(imageLabel);
-        /*
-        // 이미지 레이블 초기화
-        updateImage();
-
-        // 이미지 변경 타이머 생성
-        imageTimer = new Timer(3000, new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                currentImageIndex = (currentImageIndex + 1) % imagePaths.length;
-                updateImage();
-            }
-        });
-*/
-        // 타이머 시작
-        imageTimer.start();
         
         add(btnEnd);
         add(btnBack);
@@ -149,7 +158,7 @@ public class Home extends JPanel {
                 
             }
         });
-        
+        */
         btnSearch.addActionListener(new ActionListener() {
 			
 			@Override
@@ -197,14 +206,6 @@ public class Home extends JPanel {
 
         
     }
-    /*
-    private void updateImage() {
-        if (currentImageIndex >= 0 && currentImageIndex < imagePaths.length) {
-            ImageIcon imageIcon = new ImageIcon(imagePaths[currentImageIndex]);
-            imageIcon.setImage(imageIcon.getImage().getScaledInstance(400, 400, Image.SCALE_DEFAULT)); // 이미지 크기 조절
-            imageLabel.setIcon(imageIcon);
-            
-        }
-    }*/
+
 
 }
--- example_sc/src/main/ArtPopupMain.java
+++ example_sc/src/main/ArtPopupMain.java
@@ -2,12 +2,13 @@ package main;
 
 import frame.FrameBase;
 import frame.FrameBegin;
+import frame.Home;
 
 
 
 public class ArtPopupMain {
 	public static void main(String[] args) {
 		
-		FrameBase.getInstance(new FrameBegin());
+		FrameBase.getInstance(new Home());
 	}
 }
